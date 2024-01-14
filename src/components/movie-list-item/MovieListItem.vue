113eb2061375a164e470668eb8be7e6e2089bfc4
Universal0090 <idealxorezm@gmail.com>
2024-01-14 19:00:40 +0500

    frist change in move

---
+++ src/components/movie-list-item/MovieListItem.vue
@@ -0,0 +1,11 @@
+<template>
+    <li>Empier of osman</li>
+    <li>Omar</li>
+    <li>Ertugrul</li>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
---
+++ src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,17 @@
+<template>
+    <input type="text" class="form-control search-input" placeholder="Kinolarni qidirish"/>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.search-input {
+    margin-bottom:1rem;
+    padding:1.5rem;
+    border-radius:4px;
+    background-color: #fcfaf5;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, .15);
+}
+</style>
